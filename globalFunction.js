fc23af146c9a28e95432567ee785e5350b3043db
AlejandroFadim <alejandro.fadim@sptech.school>
2025-03-15 19:31:16 -0300

    formularos versao desk finalizada

--- globalFunction.js
+++ globalFunction.js
@@ -1,16 +1,13 @@
-
-
 export function errorCampo(mensagem){
     errosForm.style.display = 'block'; 
     errosForm.innerHTML += `${mensagem} <br>`;
-    const camposForm = document.querySelectorAll(".input-container")
-    camposForm.forEach(e =>{
-        const inputs = e.querySelectorAll('input')
-        inputs.forEach(e =>{
+    const camposForm = document.querySelectorAll(".input-container");
+    camposForm.forEach(e => {
+        const inputs = e.querySelectorAll('input');
+        inputs.forEach(e => {
             e.style.border = '1px solid red';
-
-        })
-    })
+        });
+    });
 }
 
 export function limparCampos(){
@@ -24,26 +21,35 @@ export function limparCampos(){
     });
 }
 
-
 export function carrossel() {
     let currentIndex = 0;
     const images = document.querySelectorAll('.carroselImgs .cardImgs');
     const totalImages = images.length;
+    const indicators = document.querySelectorAll('.carousel-indicators .indicator');
 
     function showNextImage() {
         currentIndex = (currentIndex + 1) % totalImages;
         const offset = -currentIndex * 100 / totalImages;
         document.querySelector('.carroselImgs').style.transform = `translateX(${offset}%)`;
+        updateIndicators(currentIndex);
     }
 
     function showPrevImage() {
         currentIndex = (currentIndex - 1 + totalImages) % totalImages;
         const offset = -currentIndex * 100 / totalImages;
         document.querySelector('.carroselImgs').style.transform = `translateX(${offset}%)`;
+        updateIndicators(currentIndex);
     }
 
-    // document.getElementById('nextButton').addEventListener('click', showNextImage);
-    // document.getElementById('prevButton').addEventListener('click', showPrevImage);
+    function updateIndicators(currentIndex) {
+        indicators.forEach((indicator, index) => {
+            if (index === currentIndex) {
+                indicator.classList.add('active');
+            } else {
+                indicator.classList.remove('active');
+            }
+        });
+    }
 
     setInterval(showNextImage, 3000);
 }
